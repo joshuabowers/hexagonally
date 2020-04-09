1eeb1e2dc1a781e4719c601c35dba42f13e1794b
Joshua Bowers <joshua.bowers+code@gmail.com>
2020-04-09 16:56:59 -0700

    Added in tslint and prettier as devDependencies

--- src/grid.ts
+++ src/grid.ts
@@ -1,3 +1,1 @@
-export class Grid {
-  
-}
+export class Grid {}
--- src/hex.ts
+++ src/hex.ts
@@ -1,26 +1,25 @@
 export enum Offset {
-  even  = +1,
-  odd   = -1
+  even = +1,
+  odd = -1,
 }
 
-export type Point = { x: number, y: number };
+export type Point = { x: number; y: number };
 
-export type Cuboid = { q: number, r: number, s: number };
-export type Axial = { q: number, r: number };
-export type Cartesian = { col: number, row: number, offset: Offset };
+export type Cuboid = { q: number; r: number; s: number };
+export type Axial = { q: number; r: number };
+export type Cartesian = { col: number; row: number; offset: Offset };
 
 export type Coordinates = Cuboid | Axial | Cartesian;
-export function isCuboid( x: Coordinates ): x is Cuboid {
-
-  return "s" in x;
+export function isCuboid(x: Coordinates): x is Cuboid {
+  return 's' in x;
 }
 
-export function isAxial( x: Coordinates ): x is Axial {
-  return !("s" in x);
+export function isAxial(x: Coordinates): x is Axial {
+  return !('s' in x);
 }
 
-export function isCartesian( x: Coordinates ): x is Cartesian {
-  return "col" in x;
+export function isCartesian(x: Coordinates): x is Cartesian {
+  return 'row' in x && 'col' in x && 'offset' in x;
 }
 
 /**
@@ -31,40 +30,40 @@ export class Hex<TValue> {
   coordinates: Cuboid;
   value: TValue;
 
-  constructor( coordinates: Coordinates, value: TValue ){
-    this.coordinates = Hex.detectCoordinates( coordinates );
+  constructor(coordinates: Coordinates, value: TValue) {
+    this.coordinates = Hex.detectCoordinates(coordinates);
     this.value = value;
   }
 
   toPoint(): Point {
     return {
       x: 0,
-      y: 0
-    }
+      y: 0,
+    };
   }
 
-  private static detectCoordinates( coordinates: Coordinates ): Cuboid {
-    if( isAxial( coordinates ) ){
-      return Hex.fromAxial( coordinates );
-    } else if( isCartesian( coordinates ) ){
-      return Hex.fromCartesian( coordinates );
+  private static detectCoordinates(coordinates: Coordinates): Cuboid {
+    if (isAxial(coordinates)) {
+      return Hex.fromAxial(coordinates);
+    } else if (isCartesian(coordinates)) {
+      return Hex.fromCartesian(coordinates);
     } else {
       return coordinates;
     }
   }
 
-  private static fromAxial( coordinates: Axial ): Cuboid {
+  private static fromAxial(coordinates: Axial): Cuboid {
     return {
       q: coordinates.q,
       r: coordinates.r,
-      s: -coordinates.q - coordinates.r
-    }
+      s: -coordinates.q - coordinates.r,
+    };
   }
 
-  private static fromCartesian( coordinates: Cartesian ): Cuboid {
+  private static fromCartesian(coordinates: Cartesian): Cuboid {
     return Hex.fromAxial({
       q: coordinates.col,
-      r: coordinates.row - (coordinates.col + coordinates.offset * (coordinates.col & 1)) / 2
+      r: coordinates.row - (coordinates.col + coordinates.offset * (coordinates.col & 1)) / 2,
     });
   }
 }
